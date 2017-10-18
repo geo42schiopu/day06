ad9facd020af3fc9bc35063868ce5269f2654b25
Georgian SCHIOPU <gschiopu@b1p13.academyplus.ro>
2017-10-18 23:27:39 +0300

    all4

--- ex01/ft_print_program_name.c
+++ ex01/ft_print_program_name.c
@@ -1,8 +1,20 @@
-void ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiopu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/18 22:57:35 by gschiopu          #+#    #+#             */
+/*   Updated: 2017/10/18 22:59:20 by gschiopu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_putstr(char *str)
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
@@ -12,10 +24,9 @@ void ft_putstr(char *str)
 	}
 }
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	ft_putstr(argv[0]);
 	ft_putchar('\n');
 	return (0);
 }
-
--- ex02/ft_print_params.c
+++ ex02/ft_print_params.c
@@ -1,8 +1,20 @@
-void ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiopu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/18 23:08:32 by gschiopu          #+#    #+#             */
+/*   Updated: 2017/10/18 23:13:06 by gschiopu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_putstr(char *str)
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
@@ -12,17 +24,16 @@ void ft_putstr(char *str)
 	}
 }
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	int	i;
-	
-	i = 2;	
+
+	i = 2;
 	while (i <= argc)
-	{	
-		ft_putstr(argv[i-1]);
+	{
+		ft_putstr(argv[i - 1]);
 		ft_putchar('\n');
 		i++;
 	}
 	return (0);
 }
-
